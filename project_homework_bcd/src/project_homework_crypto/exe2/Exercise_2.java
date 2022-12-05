401e170964199de8a271f08ee9906cc9106c6071
kimlee <kimlee@10.111.66.242>
2022-12-05 18:02:31 +0800

    homework/exercise/2

---
+++ project_homework_bcd/src/project_homework_crypto/exe2/Exercise_2.java
@@ -0,0 +1,31 @@
+package project_homework_crypto.exe2;
+
+public class Exercise_2 {
+
+	/**
+	 * 
+	 * Exercise:
+	 * 
+	 * 	File: keanggotaan-majlis-perubatan-tradisional-dan-komplementari-ptk.xlsx
+	 * 	Output: ListPractitioner-sample.txt	
+	 * 
+	 * 	Create a program methods that create ID Address for the given records.
+	 *
+	 *		- read the given dataset from the excel file.
+	 *		- generate encrypted ID for the record using symmetric key.
+	 *      - write the records with Encrypted ID to the new file.
+	 *
+	 *  Given the dataset. Encrypt the name
+	 *  	ie., NAME											ID
+	 *  		 Datuk Dr. Noor Hisham bin Abdullah				HiB0kAwhuW9OBoVTFXZjlAQ8OUjiJSRBU21qmzAhzQk=
+	 *  
+	 *  Support:
+	 *    - Java API for MS document
+	 *    	https://poi.apache.org/
+	 *    
+	 *    - Tutorial for reading/writing MS document
+	 *    	https://howtodoinjava.com/java/library/readingwriting-excel-files-in-java-poi-tutorial/
+	 *     
+	 */
+	
+}
